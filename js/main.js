5734917bf32c03e4fdc11b40642e6aeec8591340
Hamilton <hamiltonnjunior@gmail.com>
2024-11-07 18:30:34 -0300

    Incremento da funcionalidade de envio do formulário contato pelo formsubmit

--- js/main.js
+++ js/main.js
@@ -18,11 +18,52 @@ $(document).ready(function(){
         },1500)
     })
 
-       $(window).scroll(function() {
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             $('#voltar-topo').fadeIn();
         } else {
             $('#voltar-topo').fadeOut();
         }
     });
+
+    $('form').validate({
+        rules:{
+            nome:{
+                required:true
+            },
+            email:{
+                required:true,
+                email:true
+            },
+            telefone:{
+                required:true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome',
+            email: 'Por favor, insira um e-mail válido',
+            telefone: 'Por favor, insira um telefone'
+        },
+        submitHandler: function(form){
+            $.ajax({
+                url: $(this).attr('action'),
+                type: 'POST',
+                data: $(this).serialize(),
+                success: function(response) {
+                  console.log('Formulário enviado com sucesso:', response);
+                  // Adicione aqui o que você deseja fazer após o envio (ex: mostrar uma mensagem de sucesso)
+                  $('form')[0].reset();
+                },
+                error: function(xhr, status, error) {
+                  console.error('Erro ao enviar o formulário:', error);
+                }
+              });
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }
+    })
 })
